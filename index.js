72f3fca7539c5f999998cd8ebe09263f5f7b6b88
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-05-03 17:31:53 -0700

    Added seed files and project router/model

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const server = require('./server');
-// const projectRouter = require('./projects/project-router');
+const projectRouter = require('./projects/project-router');
 // const resourceRouter = require('./resources/resource-router')
 // const taskRouter = require('./tasks/task-router')
 
 const PORT = process.env.PORT || 4000;
 
-// server.use('/api/projects', projectRouter);
+server.use('/api/projects', projectRouter);
 // server.use('/api/resources', resourceRouter);
 // server.use('/api/tasks', taskRouter);
 
